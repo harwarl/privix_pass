00ce93aff1d624407e659ef9472fcec4ccd75889
harwarl <harwarl87@gmail.com>
2025-02-17 13:45:47 +0100

    feat: function to set the users IPFS hash

--- backend/script/Pass.sol
+++ backend/script/Pass.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract PassByPrivix {
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract PassByPrivix is ReentrancyGuard {
     /*Errors*/
     error PassByPrivix__UserDoesNotExist();
+    error PassByPrivix__HashCannotBeEmpty();
+    error PassByPrivix__UpdateCantBeDoneNow();
 
     /*Type Declarations*/
     struct User {
-        bytes32 ipfsHash; //IPFS hash for encrypoted password data
+        bytes32 ipfsHash; //IPFS hash for encrypted password data
         bool exists; // Check if user exists
     }
 
-    mapping (address => User) private s_users;
+    mapping (address => User) private s_users; 
     mapping (address => uint256) private s_lastTimeStamp;
 
     uint256 private i_interval;
@@ -20,5 +24,28 @@ contract PassByPrivix {
     event UserRegistered(address indexed user, bytes32 ipfsHash);
     event IPFSHashUpdated(address indexed user, bytes32 oldHash, bytes32 newHash);
 
+    constructor(
+        uint256 interval
+    ){
+        i_interval = interval;
+    }
 
+    /**
+     * @param _ipfsHash The Hash from the saved Password on IPFS
+     */
+    function setUserIPFSHash(bytes32 _ipfsHash) public nonReentrant {
+        if(_ipfsHash == bytes32(0)){
+            revert PassByPrivix__HashCannotBeEmpty();
+        }
+        if( block.timestamp < s_lastTimeStamp[msg.sender] + i_interval){
+            revert PassByPrivix__UpdateCantBeDoneNow();
+        }
+        bytes32 oldHash = s_users[msg.sender].ipfsHash;
+        s_users[msg.sender] = User({
+            ipfsHash: _ipfsHash,
+            exists: true
+        });
+        s_lastTimeStamp[msg.sender] = block.timestamp;
+        emit IPFSHashUpdated(msg.sender, oldHash, _ipfsHash);
+    }
 }
