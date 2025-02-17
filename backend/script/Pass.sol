d4fb88afb5036dc757307e681c7a28d072e2dc12
harwarl <harwarl87@gmail.com>
2025-02-17 13:28:22 +0100

    feat: variable declaration

--- backend/script/Pass.sol
+++ backend/script/Pass.sol
@@ -2,12 +2,23 @@
 pragma solidity ^0.8.18;
 
 contract PassByPrivix {
+    /*Errors*/
+    error PassByPrivix__UserDoesNotExist();
+
+    /*Type Declarations*/
     struct User {
         bytes32 ipfsHash; //IPFS hash for encrypoted password data
         bool exists; // Check if user exists
     }
 
     mapping (address => User) private s_users;
-    mapping (address => uint256) private s_lastUpdateTime;
-    
+    mapping (address => uint256) private s_lastTimeStamp;
+
+    uint256 private i_interval;
+
+    /*Events*/
+    event UserRegistered(address indexed user, bytes32 ipfsHash);
+    event IPFSHashUpdated(address indexed user, bytes32 oldHash, bytes32 newHash);
+
+
 }
