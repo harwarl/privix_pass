dc836fcd68abc5ecfa0e806df01ad1ec51927f33
harwarl <harwarl87@gmail.com>
2025-02-19 17:29:13 +0100

    setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Tomorrow } from "next/font/google";
+import "./globals.css";
+import { APP_DESCRIPTION, APP_NAME } from "@/data/constants";
+
+const tomorrow = Tomorrow({
+  variable: "--font-tomorrow",
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${tomorrow.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/data/constants.ts
@@ -0,0 +1,4 @@
+import "dotenv/config";
+
+export const APP_NAME = "Pass";
+export const APP_DESCRIPTION = "Decentralizd Password Storage";
