ec177639c7f669d0ce6c70103991e1c87bcfa2e2
harwarl <harwarl87@gmail.com>
2025-02-23 00:13:24 +0100

    chore: animation to dashboard click

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { THIRDWEB_CLIENT } from "@/services/web3";
-import { Box, Card } from "@radix-ui/themes";
-import { Shield, Lock, Database } from "lucide-react";
+import { Box, Card, Code } from "@radix-ui/themes";
+import {
+  Shield,
+  Lock,
+  Database,
+  LogOut,
+  Copy,
+  CircleArrowRight,
+} from "lucide-react";
 import { NextPage } from "next";
 import React from "react";
 import {
@@ -11,58 +18,72 @@ import {
   useDisconnect,
 } from "thirdweb/react";
 import { createWallet } from "thirdweb/wallets";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   const account = useActiveAccount();
   const { disconnect } = useDisconnect();
   const wallet = useActiveWallet();
 
+  const handleDisconnect = () => {
+    if (wallet) {
+      disconnect(wallet);
+    }
+  };
+
   return (
-    <div className="bg-background min-h-screen flex items-center justify-center p-4">
-      <div
+    <div className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 flex items-center justify-center p-4">
+      {/* <div
         style={{
           background: `linear-gradient(to bottom right, rgba(69, 172, 171, 0.1), rgba(71, 136, 172, 0.1))`,
         }}
         className="absolute inset-0"
-      />
+      /> */}
       <Box maxWidth="480px">
-        <Card className="bg-card relative z-10 max-w-md w-full p-8 text-center border-0 rounded-md">
+        <Card className="relative z-10 max-w-md w-full bg-slate-900/50 backdrop-blur border-slate-700 p-8 text-center border-0 rounded-lg">
           <div className="flex justify-center mb-6">
             <div className="bg-secondary p-3 rounded-full">
               <Shield className="h-12 w-12 text-primary" />
             </div>
           </div>
-          <h1 className="text-foreground text-3xl font-bold mb-3">
+          <h1 className="text-foreground text-3xl font-bold my-4">
             Pass By Privix
           </h1>
           <p className="text-textPrimary mb-6">
             Your decentralized gateway to true digital sovereignty
           </p>
-          <div className="space-y-4 mb-8">
-            <div className="flex items-center justify-center gap-2 text-sm">
+          <div className="space-y-4 mb-8 text-sm">
+            <div className="flex items-center gap-3 p-3 rounded-lg bg-slate-800/50">
               <Lock className="text-primary h-4 w-4" />
               <span className="text-textSecondary">
                 No central point of failure
               </span>
             </div>
-            <div className="flex items-center justify-center gap-2 text-sm">
+            <div className="flex items-center gap-3 p-3 rounded-lg bg-slate-800/50">
               <Database className="text-primary h-4 w-4" />
               <span className="text-textSecondary">
                 Your data, your control
               </span>
             </div>
           </div>
-          {/* <button className="btn btn-primary">Get Started</button>
-           */}
           {account ? (
-            <div
-              className="btn btn-primary"
-              onClick={() => disconnect(wallet!)}
-            >
-              <h2 className="text-lg font-bold">
-                {" "}
-                {account.address.slice(0, 6)}...{account.address.slice(-6)}
-              </h2>
+            <div className="space-y-4">
+              <div className="flex items-center justify-between p-4 bg-slate-800/50 rounded-lg">
+                <Code className="font-mono text-primary">
+                  {account.address.slice(0, 6)}...{account.address.slice(-6)}
+                </Code>
+                <div className="flex gap-2">
+                  <button className="p-2 rounded-lg ">
+                    <Copy className="h-4 w-4 text-muted-foreground hover:text-primary transition-colors" />
+                  </button>
+                  <button
+                    onClick={handleDisconnect}
+                    className="p-2 rounded-lg "
+                  >
+                    <LogOut className="h-4 w-4 text-muted-foreground hover:text-primary transition-colors" />
+                  </button>
+                </div>
+              </div>
             </div>
           ) : (
             <ConnectButton
@@ -76,11 +97,12 @@ const Home: NextPage = () => {
               connectButton={{
                 label: "Connect Wallet",
                 style: {
+                  fontSize: "16px",
                   backgroundColor: "var(--btnPrimary)",
                   color: "var(--foreground)",
                   width: "100%",
-                  padding: "12px 24px",
-                  borderRadius: "16px",
+                  padding: "10px 24px",
+                  borderRadius: "10px",
                   fontWeight: "500",
                   border: "1px solid var(--primary)",
                   transition: "background-color 0.3s ease-in-out",
@@ -89,6 +111,20 @@ const Home: NextPage = () => {
             />
           )}
 
+          <div className="my-3">
+            <motion.button
+              initial={{ y: 0 }}
+              animate={{ y: [0, -5, 0] }}
+              transition={{
+                repeat: Infinity,
+                duration: 1.2,
+                ease: "easeInOut",
+              }}
+            >
+              <CircleArrowRight className="h-10 w-10 text-primary" />
+            </motion.button>
+            <p className="text-xs text-primary">Click to go to dashboard</p>
+          </div>
           <p className="text-textSecondary text-xs mt-4">
             Fully decentralized. Forever private.
           </p>
